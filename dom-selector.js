3267436d2d39e091fd41eb91f8086cbc124771ba
sguha-fc <sahasrangshu@fusioncharts.com>
2015-07-29 13:48:18 +0530

    changes

--- dom-selector.js
+++
@@ -1,8 +0,0 @@
-__ = (function(query) {
-    console.log(query);
-});
-__.construct = (function(signature){
-    console.log("construct called with "+signature);
-    signature = __;
-    return signature;
-})
