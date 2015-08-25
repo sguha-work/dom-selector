420509783d8485e78e84d62a9e98ab58e2d237cb
Sahasrangshu Guha <sguha1988.life@gmail.com>
2015-08-25 10:49:07 +0530

    Update domSelect.js

--- domSelect.js
+++ domSelect.js
@@ -1,7 +1,18 @@
-domSelect= (function(query) {
-    console.log(query);
+window.__ = domSelect= (function(queryString) {
+    
 });
-domSelect.construct = (function(signature){
+
+var QueryParser = (function() {
+	var queryDelimeter = " ";// queries should be space separated
+	this.splitQueryStringToProduceQueryArray = (function(queryString) {
+		return queryString.split(queryDelimeter);
+	});
+});
+
+/**
+* This method should be called if __ is being used 
+* for another library or function
+*/
+domSelect.construct = (function(){
     return domSelect;
-})
-__ = domSelect;
+});
