bd517ec5c49fb85dd84b4953c4f5f5b7e2342090
sguha-fc <sahasrangshu@fusioncharts.com>
2015-08-26 16:59:49 +0530

    changes

--- domSelect.js
+++ domSelect.js
@@ -1,18 +1,34 @@
-window.__ = domSelect= (function(queryString) {
-    
+window.__ = domSelect = (function(queryString) {
+    var qp = new QueryParser(),
+        queryArray = qp.splitQueryStringToProduceQueryArray(queryString),
+        doms = qp.executeQueries(queryArray);
+    return ((doms.length>1)?doms:(doms.length?doms[0]:0));
 });
 
 var QueryParser = (function() {
-	var queryDelimeter = " ";// queries should be space separated
-	this.splitQueryStringToProduceQueryArray = (function(queryString) {
-		return queryString.split(queryDelimeter);
-	});
+
+    var queryDelimeter = " ", // queries should be space separated
+    	getDomFileteredByQuery = (function(doms, query) {
+
+    	});	
+
+    this.splitQueryStringToProduceQueryArray = (function(queryString) {
+        return queryString.split(queryDelimeter);
+    });
+
+    this.executeQueries = (function(queryArray) {
+    	var index, doms;
+    	for(index in queryArray) {
+    		getDomFileteredByQuery(doms, queryArray[index]);
+    	}
+    });
+
 });
 
 /**
-* This method should be called if __ is being used 
-* for another library or function
-*/
-domSelect.construct = (function(){
+ * This method should be called if __ is being used 
+ * for another library or function
+ */
+domSelect.construct = (function() {
     return domSelect;
 });
