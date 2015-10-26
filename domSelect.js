7926307a4a59ffa520c3101995fc249321223dba
sguha-work <sahasrangshu@fusioncharts.com>
2015-10-26 18:43:20 +0530

    changes

--- domSelect.js
+++ domSelect.js
@@ -4,8 +4,30 @@ window.__ = domSelect = (function(queryString) {
     var QueryParser = (function() {
 
         var queryDelimeter = " ", // queries should be space separated
-            getDomFileteredByQuery = (function(doms, query) {
+            selectDomsByClassName = (function(doms, className){
+                if(doms == null) {console.log(document.getElementsByClassName(className));
+                    return document.getElementsByClassName(className);
+                } else {
 
+                }
+            }),
+            getDomFilteredByQuery = (function(doms, query) {
+                if(query[0]=="." || query[0]=="#" || query[0]=="_") {
+                    switch (query[0]) {
+                        case ".":
+                            doms = selectDomsByClassName(doms, query.substr(1, (query.length-1)));
+                        break;
+                        case "#":
+                        break;
+                        case "_":
+                        break;
+                    }
+                } else {
+                    if(doms == null) {
+                        doms = document.getElementsByTagName("*");
+                    }
+                }
+                return doms;
             }); 
 
         this.splitQueryStringToProduceQueryArray = (function(queryString) {
@@ -13,10 +35,11 @@ window.__ = domSelect = (function(queryString) {
         });
 
         this.executeQueries = (function(queryArray) {
-            var index, doms;
+            var index, doms=null;
             for(index in queryArray) {
-                getDomFileteredByQuery(doms, queryArray[index]);
+                doms = getDomFilteredByQuery(doms, queryArray[index]);
             }
+            return doms;
         });
 
     });
@@ -26,7 +49,8 @@ window.__ = domSelect = (function(queryString) {
         queryArray = qp.splitQueryStringToProduceQueryArray(queryString),
         doms = qp.executeQueries(queryArray);
     if(typeof doms != "undefined") {
-        return ((doms.length>1)?doms:(doms.length?doms[0]:0));
+        //return ((doms.length>1)?doms:(doms.length?doms[0]:0));
+        return doms;
     } else {
         return false;
     }
