1adfcecb7e623f74aa6def181e25a6c94c0169b4
Sahasrangshu Guha <sguha1988.life@gmail.com>
2015-10-27 12:41:52 +0530

    Update domSelect.js

--- domSelect.js
+++ domSelect.js
@@ -4,12 +4,55 @@ window.__ = domSelect = (function(queryString) {
     var QueryParser = (function() {
 
         var queryDelimeter = " ", // queries should be space separated
+
+            /**
+            * This method selects DOMs by class name
+            */
             selectDomsByClassName = (function(doms, className){
-                if(doms == null) {console.log(document.getElementsByClassName(className));
+                var index, tempDomsArray;
+                if(doms == null) {
                     return document.getElementsByClassName(className);
                 } else {
-
+                    for(index=0; index<doms.length; index++) {
+                        if(doms[index].className.indexOf(className)!=-1) {
+                            tempDomsArray.push(doms[index]);
+                        }
+                    }
+                    return tempDomsArray;
                 }
+            }),
+
+            /**
+            * This method selects DOMs by Id
+            */
+            selectDomById = (function(doms, id){
+                var index, tempDomsArray;
+                if(doms == null) {
+                    return document.getElementById(id);
+                } else {
+                    for(index=0; index<doms.length; index++) {
+                        if(doms[index].id==id) {
+                            return doms[index];
+                        }
+                    }
+                }
+            }),
+
+            /**
+            * This method selects DOMs by tag name
+            */
+            selectDomsByTagName = (function(doms, query){
+               var index, tempDomsArray;
+                if(doms == null) {
+                    return document.getElementsByTagName(query);
+                } else {
+                    for(index=0; index<doms.length; index++) {
+                        if(doms[index].tagName.toLowerCase().indexOf(query.toLowerCase())!=-1) {
+                            tempDomsArray.push(doms[index]);
+                        }   
+                    }
+                    return tempDomsArray;
+                } 
             }),
             getDomFilteredByQuery = (function(doms, query) {
                 if(query[0]=="." || query[0]=="#" || query[0]=="_") {
@@ -18,14 +61,13 @@ window.__ = domSelect = (function(queryString) {
                             doms = selectDomsByClassName(doms, query.substr(1, (query.length-1)));
                         break;
                         case "#":
+                            doms = selectDomById(doms, query.substr(1, (query.length-1)));
                         break;
                         case "_":
                         break;
                     }
                 } else {
-                    if(doms == null) {
-                        doms = document.getElementsByTagName("*");
-                    }
+                    doms = selectDomsByTagName(doms, query);
                 }
                 return doms;
             }); 
